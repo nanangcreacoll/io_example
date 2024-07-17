d85069a1068cb8aeab63dac2a32a727e7c2b307f
nanangcreacoll <nanang.creacol.26@gmail.com>
2024-07-17 17:12:49 +0700

    feat: serial port com

--- app/src/main/java/com/example/io_example/SerialActivity.kt
+++ app/src/main/java/com/example/io_example/SerialActivity.kt
@@ -1,13 +1,68 @@
 package com.example.io_example
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import com.example.io_example.io.Serial
+import java.io.IOException
+import java.security.InvalidParameterException
 
 class SerialActivity : ComponentActivity() {
+    private val serialPortPath = "/dev/ttyACM0"
+    private val baudRate = 115200
+    private lateinit var serialHelper: Serial
+
+    private lateinit var sendButton: Button
+    private lateinit var input1: EditText
+    private lateinit var input2: EditText
+    private lateinit var result: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_serial)
+
+        sendButton = findViewById(R.id.serialSendButton)
+        input1 = findViewById(R.id.input1Number)
+        input2 = findViewById(R.id.input2Number)
+        result = findViewById(R.id.serialResultView)
+
+        serialHelper = Serial(serialPortPath, baudRate)
+
+        try {
+            serialHelper.open()
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } catch (e: InvalidParameterException) {
+            e.printStackTrace()
+        }
+
+        sendButton.setOnClickListener {
+            sendData(input1.text.toString(), input2.text.toString(), serialHelper)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serialHelper.close()
+    }
+
+    private fun sendData(input1: String, input2: String, serial: Serial) {
+        if (serial.isOpen) {
+            val sendData = "$input1 $input2"
+            serial.sendTxt("$sendData\r\n")
+            Log.d("SerialData", "Sent data: $sendData, with CRLF")
+            while (serialHelper.data.isEmpty()) {}
+            result.text = serialHelper.data
+            serialHelper.data = ""
+        } else {
+            Log.e("SerialError", "Serial port is not open")
+        }
     }
 }
--- app/src/main/java/com/example/io_example/io/Serial.kt
+++ app/src/main/java/com/example/io_example/io/Serial.kt
@@ -1,4 +1,15 @@
 package com.example.io_example.io
 
-class Serial {
+import android.util.Log
+import tp.xmaihh.serialport.SerialHelper
+import tp.xmaihh.serialport.bean.ComBean
+
+class Serial (port: String, baudRate: Int): SerialHelper(port, baudRate){
+    var data: String = ""
+
+    public override fun onDataReceived(comBean: ComBean) {
+        val receivedData = String(comBean.bRec)
+        Log.d("SerialData", "Received data: $receivedData")
+        data = receivedData
+    }
 }
